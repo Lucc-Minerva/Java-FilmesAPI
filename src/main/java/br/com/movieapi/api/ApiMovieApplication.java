9f6def58bbb4d5534ba59fb6e3eb6c592c903153
Lucas Gonçalves <103429796+Lucc-Minerva@users.noreply.github.com>
2024-06-18 15:45:32 +0000

    feat: separando a key para uma variavel apikey

--- src/main/java/br/com/movieapi/api/ApiMovieApplication.java
+++ src/main/java/br/com/movieapi/api/ApiMovieApplication.java
@@ -8,12 +8,14 @@ import java.net.http.HttpResponse;
 
 public class ApiMovieApplication {
     public static void main(String[] args) throws IOException, InterruptedException {
-        
+    
+    String apiKey = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyZDVmNjc2ZWI2MmVhMzI4ODZhZGMzYjJiNzZlMTc3MyIsInN1YiI6IjY2NzA5YjMyZjk4NGRlZjhiN2MzNWEzYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.sPZf6AUvWGIaCueAzD0vqBWrgWt4FepYYxUcvJeq7DM";
+
     HttpRequest request = HttpRequest
     .newBuilder()
     .uri(URI.create("https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1"))
     .header("accept", "application/json")
-    .header("Authorization", "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyZDVmNjc2ZWI2MmVhMzI4ODZhZGMzYjJiNzZlMTc3MyIsInN1YiI6IjY2NzA5YjMyZjk4NGRlZjhiN2MzNWEzYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.sPZf6AUvWGIaCueAzD0vqBWrgWt4FepYYxUcvJeq7DM")
+    .header("Authorization", "Bearer " + apiKey)
     .method("GET", HttpRequest.BodyPublishers.noBody())
     .build();
     HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
